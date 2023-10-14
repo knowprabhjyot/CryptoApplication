bb8f0f9dba2c1ccbcd1e0be45a66c246b3127bcb
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2023-10-14 15:18:03 -0700

    Refs #8 Search

--- src/components/CryptoTable.jsx
+++ src/components/CryptoTable.jsx
@@ -1,37 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import { Table } from 'react-bootstrap'
+import { Form, InputGroup, Table } from 'react-bootstrap'
 import CryptoTableRow from './CryptoTableRow'
 import './CryptoTable.css';
 
-const   CryptoTable = ({ data }) => {
+const CryptoTable = ({ data }) => {
+
+
+  const [searchedKeyword, setSearchedKeyword] = useState('');
+  const [tableData, setTableData] = useState(data);
+  const [tempData, _setTempData] = useState(data);
+
+  useEffect(() => {
+    
+    // console.log(searchedKeyword, 'searchedKeyword');
+    setTableData((prevState) => {
+      return tempData;
+    });
+    const filteredArray = tableData.filter((coin) => {
+      return coin.name.includes(searchedKeyword);
+    })
+    setTableData(filteredArray);
+  }, [searchedKeyword])
+
+  useEffect(() => {
+    setTableData(data);
+  }, [data]);
+
   return (
-    <Table className='table-container' striped bordered hover>
-      <thead>
-        <tr>
-          <th>AVATAR</th>
-          <th>Name</th>
-          <th>Symbol</th>
-          <th>Current price</th>
-          <th>24 HIGH</th>
-          <th>24 LOW</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-            data.map((coinData, index) => {
-                return <CryptoTableRow data={coinData} key={index} />
-                // Create A table Row Component
+    <div>
+      <InputGroup style={{ width: '800'}} size="sm" className="mb-3 p-3">
+        <InputGroup.Text id="inputGroup-sizing-sm">Search</InputGroup.Text>
+        <Form.Control
+          aria-label="Small"
+          aria-describedby="inputGroup-sizing-sm"
+          value={searchedKeyword}
+          onChange={(event) => setSearchedKeyword(event.target.value)}
+        />
+      </InputGroup>
+
+      <Table className='table-container' striped bordered hover>
+        <thead>
+          <tr>
+            <th>AVATAR</th>
+            <th>Name</th>
+            <th>Symbol</th>
+            <th>Current price</th>
+            <th>24 HIGH</th>
+            <th>24 LOW</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            tableData.map((coinData, index) => {
+              return <CryptoTableRow data={coinData} key={index} />
+              // Create A table Row Component
             })
-        }
-      </tbody>
-    </Table>
+          }
+        </tbody>
+      </Table>
+    </div>
+
   )
 }
 
 CryptoTable.propTypes = {
-    data: PropTypes.any
+  data: PropTypes.any
 }
 
 export default CryptoTable
