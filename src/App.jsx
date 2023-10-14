da6621ad847f059315b40cc64bba971abffe03da
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2023-10-14 11:36:53 -0700

    Ref #3 Coins API being called

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import { Button } from 'react-bootstrap';
 
+const URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd`;
+
 
 function App() {
+
+  const [coinsData, setCoinsData] = useState([]);
+
+  useEffect(() => {
+    // fetch(URL).then((response) => {
+    //   return response.json();
+    // }).then((data) => {
+    //   console.log(data);
+    // }).catch((error) => {
+    //   console.error(error);
+    // })
+
+    getCoinsData();
+  }, [])
+
+  const getCoinsData = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setCoinsData(data);
+  }
 
   return (
     <>
